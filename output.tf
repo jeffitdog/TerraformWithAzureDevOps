bdaf4277827a36bcf57093ab0c196e77a2288edd
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-03-05 00:51:28 +0800

    revise tf file

--- output.tf
+++ output.tf
@@ -1,9 +1,4 @@
-output "vnet" {
-  value       = module.network.vnet
-  description = "The module's vnet object."
-}
-
-output "subnets" {
-  value       = module.network.subnets
-  description = "The module's subnets object."
+output "resource_group {
+  value       = azurerm_resource_group.iac_test.name
+  description = "This is resource group."
 }
