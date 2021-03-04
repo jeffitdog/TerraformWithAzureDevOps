4b2e023fc1ac9765e6b390180968c1e28b0b026a
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-03-05 00:47:00 +0800

    revise tf file

--- terraform.tf
+++ terraform.tf
@@ -8,20 +8,4 @@ resource "azurerm_resource_group" "iac_test" {
   }
 }
 
-module "network" {
-  source                 = "github.com/terraform-azurerm-modules/terraform-azure-vnet"
-  resource_group         = azurerm_resource_group.iac_test.name
-  location               = azurerm_resource_group.iac_test.location
-  tags                   = azurerm_resource_group.iac_test.tags
 
-  vnet_name              = "hub"
-  address_space          = [ "10.0.0.0/24" ]
-  dns_servers            = [ "10.0.0.68", "10.0.0.69" ]
-
-  subnets                = {
-    AzureFirewallSubnet  = "10.0.0.0/26"
-    SharedServices       = "10.0.0.64/26"
-    AzureBastionSubnet   = "10.0.0.192/27"
-    GatewaySubnet        = "10.0.0.224/27"
-  }
-}
