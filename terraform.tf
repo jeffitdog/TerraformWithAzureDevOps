aa0ca724498ef92b8d105cc80c3a0babe9fba3ff
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-03-06 16:31:05 +0800

    Change RG name

--- terraform.tf
+++ terraform.tf
@@ -1,5 +1,6 @@
 resource "azurerm_resource_group" "iac_test" {
-  name     = "myTestResourceGroup"
+  #name     = "myTestResourceGroup"
+  name     = "ValidName"
   location = "West Europe"
 
   tags     = {
