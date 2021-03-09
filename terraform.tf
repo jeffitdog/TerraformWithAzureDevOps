56c741cf2119b11be80d391c7264e5123c1d9455
Jeffrey CF Ho <Jeffrey CF Ho@cn.asia.ad.pwcinternal.com>
2021-03-09 23:57:42 +0800

    invalid name

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 resource "azurerm_resource_group" "iac_test" {
-  #name     = "myTestResourceGroup"
-  name     = "ValidName"
+  name     = "myTestResourceGroup"
+  #name     = "ValidName"
   location = "West Europe"
 
   tags     = {
