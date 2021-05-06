88ac1df1db21434214984b2217358e869bb57227
jeffitdog <62145580+jeffitdog@users.noreply.github.com>
2021-05-06 20:22:11 +0800

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -2,7 +2,7 @@ resource "azurerm_resource_group" "iac_test" {
   name     = "Invalid_Name"
   #name     = "ValidName"
   location = "West Europe"
-
+  
   tags     = {
     environment = "dev"
     costcenter  = "it"
